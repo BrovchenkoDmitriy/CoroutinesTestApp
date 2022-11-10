cbde15909b2af21754d38a396cc5490b8b927bf1
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2022-11-10 17:13:49 +0900

    exceptionHandler and 5th rule of structured concurrency in comments

--- app/src/main/java/com/example/coroutinestestapp/MainViewModel.kt
+++ app/src/main/java/com/example/coroutinestestapp/MainViewModel.kt
@@ -3,36 +3,56 @@ package com.example.coroutinestestapp
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.*
+import java.lang.RuntimeException
 import kotlin.concurrent.thread
 
 class MainViewModel : ViewModel() {
 
     private val parentJob = Job()
 
-    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+    private val exceptionHandler = CoroutineExceptionHandler{_, throwable ->
+        Log.d(LOG_TAG, "Exception caught $throwable")
+    }
+
+
+    private val coroutineScope = CoroutineScope(
+        Dispatchers.Main + parentJob + exceptionHandler
+    )
+
 
     fun method() {
+
         val childJob1 = coroutineScope.launch {
-            delay(2000)
+            delay(3000)
             Log.d(LOG_TAG, "First coroutines finished ")
         }
+
         val childJob2 = coroutineScope.launch {
-            delay(5000)
+            delay(2000)
+            error()
+            // in this moment exception will be forwarded to the parent coroutine
+            // where it will be processed exceptionHandler
+            // and other child coroutines will be canceled (it depends  on the type of Job)
             Log.d(LOG_TAG, "Second coroutines finished ")
         }
-        thread {
-            Thread.sleep(1000)
-            parentJob.cancel()
-            Log.d(LOG_TAG, "Parent job is active: ${parentJob.isActive}")
+
+        val childJob3: Job = coroutineScope.launch {
+            delay(1000)
+            Log.d(LOG_TAG, "Third coroutines finished ")
         }
-        Log.d(LOG_TAG, "${parentJob.children.contains(childJob1)}")
-        Log.d(LOG_TAG, "${parentJob.children.contains(childJob2)}")
+
+
+    }
+
+    private fun error() {
+        throw RuntimeException()
     }
 
     override fun onCleared() {
         super.onCleared()
         coroutineScope.cancel()
     }
+
     companion object {
         private const val LOG_TAG = "MainViewModel"
     }
